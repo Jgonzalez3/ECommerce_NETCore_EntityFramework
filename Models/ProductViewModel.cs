637f9b25ca0c18a060586089f0f98bb05f9ad03f
Javier Gonzalez <javig56@gmail.com>
2018-06-15 23:35:31 -0700

    update ViewModel for validation error messages

--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -7,11 +7,13 @@ namespace E_Commerce.Models
         public string name {get;set;}
         [Required(ErrorMessage= "Product Description required")]
         public string description {get;set;}
+        [Range(0,9999)]
         [Required(ErrorMessage= "Product Quantity required")]
-        public int quantity {get;set;}
+        public int? quantity {get;set;}
         [Required(ErrorMessage= "Product Image Url required")]
         public string image {get;set;}
+        [Range(0.01, 9999.99)]
         [Required(ErrorMessage= "Product Price required")]
-        public string price {get;set;}
+        public decimal? price {get;set;}
     }
 }
